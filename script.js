421d3a9b98beaf41ab1d8954fd5122e0d56bb84e
Somnath Choudhury <102171721+somnath-choudhury@users.noreply.github.com>
2025-09-03 21:29:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", () => {
+    const outputDiv = document.getElementById("output");
+
+    const initialPromise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve([1,2,3,4])
+        }, 3000);
+    })
+
+    initialPromise.then((arr) => {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                const evens = arr.filter((n) => n % 2 === 0)
+                outputDiv.textContent = evens
+                resolve(evens)
+            }, 1000)
+        })
+    }).then((evens) => {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                const doubled = evens.map((n) => n * 2)
+                outputDiv.textContent = doubled;
+                resolve(doubled) 
+            }, 2000);
+        })
+    }).catch((err) => {
+        outputDiv.textContent = `Error ${err}`
+    })
+})
